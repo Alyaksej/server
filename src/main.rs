9c134d2407adde27e4e5c9122bd0f0c030b77358
aliaksei <alex.circuit.soft@gmail.com>
2024-07-09 20:19:41 +0300

    add send

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use tokio::net::UnixDatagram;
-use tokio::io::{Interest, Ready};
 use std::{fs, io};
 use std::os::raw::{c_int};
 use std::time::Instant;
@@ -21,8 +20,8 @@ extern {
 async fn main() -> io::Result<()> {
     const SOCKET_DATA_PATH: &str = "/tmp/socket_data.sock";
     const SOCKET_RESULT_PATH: &str = "/tmp/socket_result.sock";
-    const DATA_SIZE: usize = 2000_000_000;
-    const RESULT_SIZE: usize = 1000_000;
+    const DATA_SIZE: usize = 2_000_000_000;
+    const RESULT_SIZE: usize = 1_000_000;
     const BUFFER_THRESHOLD: usize = DATA_SIZE - 200_000;
     const STATS_PERIOD: u64 = 5;
     const MBYTES: u64 = 1_000_000;
@@ -42,20 +41,7 @@ async fn main() -> io::Result<()> {
         }
     };
 
-    if fs::metadata(SOCKET_RESULT_PATH).is_ok() {
-        if let Err(e) = fs::remove_file(SOCKET_RESULT_PATH) {
-            eprintln!("Error removing socket file: {}", e);
-            return Err(e);
-        }
-    };
-
-    let socket_result = match UnixDatagram::bind(SOCKET_RESULT_PATH){
-        Ok(socket_result) => socket_result,
-        Err(e) => {
-            eprintln!("Error binding socket result: {}", e);
-            return Err(e);
-        }
-    };
+    let socket_result = UnixDatagram::unbound()?;
 
     let mut data_vec = vec![0; DATA_SIZE];
     let data_c_ptr = data_vec.as_mut_ptr();
@@ -63,7 +49,7 @@ async fn main() -> io::Result<()> {
 
     let mut result_vec = vec![0; RESULT_SIZE];
     let result_c_ptr = result_vec.as_mut_ptr();
-    let result_max_len = 200_000;
+    let result_max_len = 100_000;
 
     let mut cnt_bytes = 0;
     let mut whole_bytes = 0;
@@ -74,42 +60,43 @@ async fn main() -> io::Result<()> {
 
     loop {
         if data_offset >= BUFFER_THRESHOLD {
-            println!("{:?}", data_vec);
+            //println!("{:?}", data_vec);
+            //println!("!!!!!!!!!!!!!!BUFFER_THRESHOLD");
             data_offset = 0;
         }
         let data_free_slice: &mut [u8] = &mut data_vec[data_offset..];
-        let ready = socket_data.ready(Interest::READABLE).await?;
-        let _ = socket_result.writable().await;
+        socket_data.readable().await?;
 
-        if ready.is_readable() {
-            match socket_data.try_recv(data_free_slice) {
-                Ok(len_recv) => {
-                    if len_recv >= data_free_slice.len() {
-                        return Err(io::Error::new(io::ErrorKind::Other, "Error receiving data: data is to long"));
-                    };
-                    let recv_pkt_num = data_free_slice[0];
-                    if recv_pkt_num != next_pkt_num {
-                        println!("!!!!! ERROR recv_pkt_num is equal!");
-                    }
-                    if recv_pkt_num == 255 {
-                        next_pkt_num = 0;
-                    } else {
-                        next_pkt_num = recv_pkt_num + 1;
-                    }
+        // if !ready.is_readable() {
+        //     continue;
+        // }
+        match socket_data.try_recv_from(data_free_slice) {
+            Ok((len_recv,addr)) => {
+                if len_recv >= data_free_slice.len() {
+                    return Err(io::Error::new(io::ErrorKind::Other, "Error receiving data: data is to long"));
+                };
+                let recv_pkt_num = data_free_slice[0];
+                if recv_pkt_num != next_pkt_num {
+                    println!("!!!!! ERROR recv_pkt_num is equal!");
+                }
+                if recv_pkt_num == 255 {
+                    next_pkt_num = 0;
+                } else {
+                    next_pkt_num = recv_pkt_num + 1;
+                }
 
-                    data_offset += len_recv;
-                    cnt_bytes += len_recv;
-                }
-                Err(ref e) if
-                e.kind() == io::ErrorKind::WouldBlock => {
-                    continue;
-                }
-                Err(e) => {
-                    eprintln!("Error receiving data: {:?}", e);
-                    return Err(e);
-                }
+                data_offset += len_recv;
+                cnt_bytes += len_recv;
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                eprintln!("Error receiving data: {:?}", e);
+                return Err(e);
             }
         }
+
         // Using of C-library
         let mut data_used_len: c_int = 0;
         let mut result_used_len: c_int = 0;
@@ -128,9 +115,22 @@ async fn main() -> io::Result<()> {
             if data_used_len > 0 {
                 data_vec.copy_within(data_used_len as usize..data_offset, 0);
                 data_offset = data_offset - data_used_len as usize;
-                socket_result.try_send(&result_vec);
+            }
+            if result_used_len > 0 {
+                match socket_result.try_send_to(&result_vec[0..99999], SOCKET_RESULT_PATH) {
+                    Ok(len_tx) => {
+                        println!("socket_result len_tx {}", len_tx);
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                        //println!("socket_result WouldBlock");
+                    }
+                    Err(e) => {
+                        eprintln!("Error send data: {:?}", e);
+                    }
+                };
             }
         }
+
         let now = Instant::now();
         if (now - stats_start_ts).as_secs() >= STATS_PERIOD {
             whole_bytes += cnt_bytes;
