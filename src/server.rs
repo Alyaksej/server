75efe6ecb31af87f7d0c5803b1b3fe30af361090
aliaksei <alex.circuit.soft@gmail.com>
2024-07-10 17:46:44 +0300

    add mods

---
+++ src/server.rs
@@ -0,0 +1,119 @@
+pub(crate) fn run_server() {
+    const SOCKET_DATA_PATH: &str = "/app/data-volume/socket_data.sock";
+    const SOCKET_RESULT_PATH: &str = "/app/data-volume/socket_result.sock";
+    const DATA_SIZE: usize = 2_000_000_000;
+    const RESULT_SIZE: usize = 1_000_000;
+    const BUFFER_THRESHOLD: usize = DATA_SIZE - 200_000;
+    const STATS_PERIOD: u64 = 5;
+    const MBYTES: u64 = 1_000_000;
+    // Remove socket before start
+    if fs::metadata(SOCKET_DATA_PATH).is_ok() {
+        if let Err(e) = fs::remove_file(SOCKET_DATA_PATH) {
+            eprintln!("Error removing socket data file: {}", e);
+            return Err(e);
+        }
+    };
+    // Create sockets
+    let socket_data = match UnixDatagram::bind(SOCKET_DATA_PATH) {
+        Ok(socket_data) => socket_data,
+        Err(e) => {
+            eprintln!("Error binding socket data: {}", e);
+            return Err(e);
+        }
+    };
+
+    let socket_result = UnixDatagram::unbound()?;
+
+    let mut data_vec = vec![0; DATA_SIZE];
+    let data_c_ptr = data_vec.as_mut_ptr();
+    let mut data_offset: usize = 0;
+
+    let mut result_vec = vec![0; RESULT_SIZE];
+    let result_c_ptr = result_vec.as_mut_ptr();
+    let result_max_len = 100_000;
+
+    let mut cnt_bytes = 0;
+    let mut whole_bytes = 0;
+    let mut stats_start_ts = Instant::now();
+    let server_start_ts = Instant::now();
+
+    let mut next_pkt_num = 0;
+
+    loop {
+        if data_offset >= BUFFER_THRESHOLD {
+            println!("{:?}", data_vec);
+            data_offset = 0;
+        }
+        let data_free_slice: &mut [u8] = &mut data_vec[data_offset..];
+        socket_data.readable().await?;
+
+        match socket_data.try_recv_from(data_free_slice) {
+            Ok((len_recv, addr)) => {
+                if len_recv >= data_free_slice.len() {
+                    return Err(io::Error::new(io::ErrorKind::Other, "Error receiving data: data is to long"));
+                };
+                let recv_pkt_num = data_free_slice[0];
+                if recv_pkt_num != next_pkt_num {
+                    println!("!!!!! ERROR recv_pkt_num is equal!");
+                }
+                if recv_pkt_num == 255 {
+                    next_pkt_num = 0;
+                } else {
+                    next_pkt_num = recv_pkt_num + 1;
+                }
+
+                data_offset += len_recv;
+                cnt_bytes += len_recv;
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                eprintln!("Error receiving data: {:?}", e);
+                return Err(e);
+            }
+        }
+
+        // Using of C-library
+        let mut data_used_len: c_int = 0;
+        let mut result_used_len: c_int = 0;
+        let data_max_len: c_int = data_offset as c_int;
+
+        unsafe {
+            array_processing(
+                data_c_ptr,
+                data_max_len,
+                &mut data_used_len,
+                result_c_ptr,
+                result_max_len,
+                &mut result_used_len,
+            );
+
+            if data_used_len > 0 {
+                data_vec.copy_within(data_used_len as usize..data_offset, 0);
+                data_offset = data_offset - data_used_len as usize;
+            }
+            if result_used_len > 0 {
+                match socket_result.try_send_to(&result_vec[0..99999], SOCKET_RESULT_PATH) {
+                    Ok(len_tx) => {
+                        println!("socket_result len_tx {}", len_tx);
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                        //println!("socket_result WouldBlock");
+                    }
+                    Err(e) => {
+                        eprintln!("Error send data: {:?}", e);
+                    }
+                };
+            }
+        }
+
+        let now = Instant::now();
+        if (now - stats_start_ts).as_secs() >= STATS_PERIOD {
+            whole_bytes += cnt_bytes;
+            println!("{} MB/sec {} MB total {} secs elapsed time ", cnt_bytes as u64 / (MBYTES * STATS_PERIOD), whole_bytes as u64 / (MBYTES * STATS_PERIOD), server_start_ts.elapsed().as_secs());
+            cnt_bytes = 0;
+            stats_start_ts = now;
+        }
+    }
+}
---
+++ src/test_client.rs
@@ -0,0 +1,67 @@
+pub(crate) fn run_client() {
+    const SOCKET_DATA_PATH: &str = "/app/data-volume/socket_data.sock";
+    const SOCKET_RESULT_PATH: &str = "/app/data-volume/socket_result.sock";
+    const BUFFER_SIZE: usize = 212_765;
+    // Remove socket before start
+    if fs::metadata(SOCKET_RESULT_PATH).is_ok() {
+        if let Err(e) = fs::remove_file(SOCKET_RESULT_PATH) {
+            eprintln!("Error removing socket result file: {}", e);
+            return Err(e);
+        }
+    };
+    // Create sockets
+    let socket_result = match UnixDatagram::bind(SOCKET_RESULT_PATH) {
+        Ok(socket_result) => socket_result,
+        Err(e) => {
+            eprintln!("Error binding socket result: {}", e);
+            return Err(e);
+        }
+    };
+
+    let socket_data = UnixDatagram::unbound()?;
+
+    let mut buffer = vec![0; BUFFER_SIZE];
+    let mut next_pkt_num = 0;
+    let mut buf = vec![0; 99_999];
+
+    for i in 0..BUFFER_SIZE {
+        buffer[i] = i as u8;
+    }
+    loop {
+        socket_data.writable().await?;
+
+        match socket_data.try_send_to(&buffer, SOCKET_DATA_PATH) {
+            Ok(n) => {
+                buffer[0] = next_pkt_num;
+                if next_pkt_num == 255 {
+                    next_pkt_num = 0;
+                } else {
+                    next_pkt_num += 1;
+                }
+                if n != BUFFER_SIZE {
+                    return Err(io::Error::new(io::ErrorKind::Other, "buffer is not equal to n"));
+                };
+                //break;
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                //println!("!!WouldBlock");
+                continue;
+            }
+            Err(e) => {
+                return Err(e);
+            }
+        }
+
+        match socket_result.try_recv_from(&mut buf) {
+            Ok((len_result, addr)) => {
+                println!("socket_result len_result: {}, addr {:?}", len_result, addr);
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                //println!("socket_result WouldBlock");
+            }
+            Err(e) => {
+                eprintln!("Error receiving data: {:?}", e);
+            }
+        }
+    }
+}
