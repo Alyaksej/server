b5cc9adc2425e3ec1954996fd691399e94c1a5f1
aliaksei <alex.circuit.soft@gmail.com>
2024-07-12 17:43:53 +0300

    fix mods it's work

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,21 @@ mod server;
 mod test_client;
 
 use std::{io};
+use std::env;
+use std::io::Error;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let serv = server::run_server();
-    let clie = test_client::run_test_client();
+    let args: Vec<String> = env::args().collect();
+    println!("!!!!!!!!!!!!!!!!!!!!!{:?}", args[1]);
+
+    if args.len() > 1 && args[1] == "mode=server" {
+        let _ =server::run_server().await;
+    } else if args.len() > 1 && args[1] == "mode=test_client" {
+        let _ = test_client::run_test_client().await;
+    } else {
+        return Err(io::Error::new(io::ErrorKind::Other, "ERROR: Invalid mode"));
+    }
+
     Ok(())
 }
